5f86dfd30a29cf4dd18d2a57a7dcd5e15f2341d0
LaBiXiaoChai <95676329+LaBiXiaoChai@users.noreply.github.com>
2022-05-18 22:59:34 +0800

    Add files via upload

---
+++ decorator_pred.py
@@ -0,0 +1,96 @@
+import random
+
+
+def value(result):
+    TP, TN, FP, FN = 0, 0, 0, 0
+    for item in result:
+        # print(item)
+        x1 = item[0]
+        x2 = item[1]
+        if x1 > (-1*x2):
+            Y = 1
+        else:
+            Y = 0
+        if item[2] == 0 and Y == 0:
+            TN += 1
+        elif item[2] == 0 and Y == 1:
+            FP += 1
+        elif item[2] == 1 and Y == 1:
+            TP += 1
+        else:
+            FN += 1
+    return TP, TN, FP, FN
+
+
+def ACC(origin):
+    def inner(*args,**kwargs):
+        res = origin(*args,**kwargs) # 调用原来的函数
+        TP, TN, FP, FN = value(res)
+        # print(TP,TN,FP,FN)
+        try:
+            print("ACC = ", ((TP + TN) / (TP + FP + FN + TN)))
+        except BaseException as e:
+            print(e)
+        return res
+    return inner
+
+
+def MCC(origin):
+    def inner(*args,**kwargs):
+        res = origin(*args,**kwargs) # 调用原来的函数
+        TP, TN, FP, FN = value(res)
+        try:
+            N = TN + TP + FN + FP
+            S = (TP + FN) / N
+            P = (TP + FP) / N
+            print("MCC = ", ((TP / N) - (S * P) / (P * S * (1 - S) * (1 - P)) ** 0.5))
+        except BaseException as e:
+            print(e)
+        return res
+    return inner
+
+
+
+def selfDefinedStructSampling(*args, **kwargs):
+    result = list()
+    try:
+        assert ('num' in kwargs.keys() or 'struct' in kwargs.keys()), \
+            "The input dictionary should contain 'num' and 'struct'"
+        for index in range(0, kwargs.get('num')):
+            element = list()
+            for key, value in kwargs.get("struct").items():
+                if key == "int":
+                    it = iter(value['datarange'])
+                    tmp = random.randint(next(it), next(it))
+                elif key == "float":
+                    it = iter(value['datarange'])
+                    tmp = random.uniform(next(it), next(it))
+                elif key == "lable":
+                    it = iter(value['datarange'])
+                    tmp = random.randint(next(it), next(it))
+                else:
+                    break
+                element.append(tmp)
+            result.append(element)
+    except BaseException as e:
+        print(e)
+    return result
+
+
+@ACC
+def pred1(struct):
+    result = selfDefinedStructSampling(**struct)
+    return result
+
+
+@MCC
+def pred2(*args, **kwargs):
+    result = selfDefinedStructSampling(*args, **kwargs)
+    return result
+
+
+if __name__ == "__main__":
+    struct = {"num": 100, "struct": {"int": {"datarange": (-200, 200)}, "float": {"datarange": (-200, 200)},
+                                   "lable": {"datarange": (0, 1)}}}
+    pred1(struct)
+    pred2(**struct)
